ad4dde323523befb17ed091c44f39811a5decc55
Davi de Carvalho <davi_carvalho96@hotmail.com>
2023-03-22 10:35:42 -0300

    feat: adicionado counter.dev

--- apps/nextjs/src/pages/_app.tsx
+++ apps/nextjs/src/pages/_app.tsx
@@ -1,5 +1,6 @@
 import "../styles/globals.css";
 import type { AppType } from "next/app";
+import Script from "next/script";
 import { ptBR } from "@clerk/localizations";
 import { ClerkProvider } from "@clerk/nextjs";
 import type { Session } from "next-auth";
@@ -15,6 +16,11 @@ const MyApp: AppType<{ session: Session | null }> = ({
   return (
     <ClerkProvider localization={ptBR} {...pageProps}>
       <SessionProvider session={session}>
+        <Script
+          src="https://cdn.counter.dev/script.js"
+          data-id="c364c5f9-523a-49bd-aa09-420f80558467"
+          data-utcoffset="-3"
+        />
         <Component {...pageProps} />
       </SessionProvider>
       <Toaster />
