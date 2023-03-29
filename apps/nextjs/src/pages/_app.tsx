7cac75a46e7314fbba3547e82950eda2d3ab9756
Davi de Carvalho <davi_carvalho96@hotmail.com>
2023-03-29 16:41:39 -0300

    feat: removendo nextauth

--- apps/nextjs/src/pages/_app.tsx
+++ apps/nextjs/src/pages/_app.tsx
@@ -1,21 +1,12 @@
 import "../styles/globals.css";
-import { useEffect, useState } from "react";
 import type { AppType } from "next/app";
 import Script from "next/script";
 import { ptBR } from "@clerk/localizations";
-import { ClerkProvider, useUser } from "@clerk/nextjs";
-import { H } from "highlight.run";
-import type { Session } from "next-auth";
-import { SessionProvider } from "next-auth/react";
+import { ClerkProvider } from "@clerk/nextjs";
 // import { AxiomReporter } from 'next-axiom';
 import { Toaster } from "react-hot-toast";
 
-import { api } from "~/utils/api";
-
-const MyApp: AppType<{ session: Session | null }> = ({
-  Component,
-  pageProps: { session, ...pageProps },
-}) => {
+const MyApp: AppType = ({ Component, pageProps: { ...pageProps } }) => {
   // const { user } = useUser();
   // const [alreadyInitiated, setAlreadyInitiated] = useState(false);
   // useEffect(() => {
@@ -37,18 +28,16 @@ const MyApp: AppType<{ session: Session | null }> = ({
   // }, [user, alreadyInitiated]);
   return (
     <ClerkProvider localization={ptBR} {...pageProps}>
-      <SessionProvider session={session}>
-        <Script
-          src="https://cdn.counter.dev/script.js"
-          data-id="c364c5f9-523a-49bd-aa09-420f80558467"
-          data-utcoffset="-3"
-        />
-        <Component {...pageProps} />
-        {/* <AxiomReporter /> */}
-      </SessionProvider>
+      <Script
+        src="https://cdn.counter.dev/script.js"
+        data-id="c364c5f9-523a-49bd-aa09-420f80558467"
+        data-utcoffset="-3"
+      />
+      <Component {...pageProps} />
+      {/* <AxiomReporter /> */}
       <Toaster />
     </ClerkProvider>
   );
 };
 
-export default api.withTRPC(MyApp);
+export default MyApp;
