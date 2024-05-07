9e863c8b503a58e952d8b30eab39dbf953b16a87
Davi de Carvalho <davi_carvalho96@hotmail.com>
2024-05-07 14:13:34 -0300

    ability of drag n drop generated schedule

--- apps/nextjs/src/components/school-calendar-grid/index.tsx
+++ apps/nextjs/src/components/school-calendar-grid/index.tsx
@@ -1,6 +1,25 @@
 "use client";
 
-import { useState } from "react";
+import { use, useEffect, useState } from "react";
+import {
+  closestCenter,
+  DndContext,
+  KeyboardSensor,
+  PointerSensor,
+  useSensor,
+  useSensors,
+} from "@dnd-kit/core";
+import {
+  arrayMove,
+  arraySwap,
+  rectSwappingStrategy,
+  SortableContext,
+  sortableKeyboardCoordinates,
+  useSortable,
+} from "@dnd-kit/sortable";
+import { CSS } from "@dnd-kit/utilities";
+
+import type { Subject, Teacher, User } from "@acme/db";
 
 import { api } from "~/trpc/react";
 import { CheckBox } from "../checkbox";
@@ -19,17 +38,19 @@ interface SchoolCalendarGridProps {
 
 type DayOfWeek = "Monday" | "Tuesday" | "Wednesday" | "Thursday" | "Friday";
 
+type ClassKey = `${DayOfWeek}_${string}-${string}_${string}_${string}`;
+
 // Função de componente
 export function SchoolCalendarGrid({ schoolId }: SchoolCalendarGridProps) {
   const [fixedClasses, setFixedClasses] = useState<string[]>([]);
   const [scheduleConfig, setScheduleConfig] = useState<
     Record<DayOfWeek, { start: string; end: string }>
   >({
-    Monday: { start: "07:00", end: "11:00" },
-    Tuesday: { start: "08:00", end: "12:50" },
-    Wednesday: { start: "07:00", end: "11:00" },
-    Thursday: { start: "08:00", end: "12:50" },
-    Friday: { start: "07:00", end: "11:00" },
+    Monday: { start: "07:00", end: "12:30" },
+    Tuesday: { start: "07:00", end: "12:30" },
+    Wednesday: { start: "07:00", end: "12:30" },
+    Thursday: { start: "07:00", end: "12:30" },
+    Friday: { start: "07:00", end: "12:30" },
   });
 
   const {
@@ -45,6 +66,11 @@ export function SchoolCalendarGrid({ schoolId }: SchoolCalendarGridProps) {
     },
     { refetchOnWindowFocus: false },
   );
+
+  const [tableSchedule, setTableSchedule] = useState<typeof schedule>(schedule);
+  useEffect(() => {
+    setTableSchedule(schedule);
+  }, [schedule]);
 
   const updateStartTime = (day: DayOfWeek, startTime: string) => {
     setScheduleConfig((prev) => ({
@@ -66,25 +92,61 @@ export function SchoolCalendarGrid({ schoolId }: SchoolCalendarGridProps) {
     }));
   };
 
-  if (isLoading) return <div className="mt-5 text-center">Loading...</div>;
-  if (error) {
-    return <div className="mt-5 text-center">Error: {error.message}</div>;
-  }
+  const [allTimeSlots, setAllTimeSlots] = useState<string[]>([]);
+  const [allClassKeys, setAllClassKeys] = useState<string[]>([]);
 
-  if (!schedule || Object.keys(schedule).length === 0)
-    return <div className="mt-5 text-center">Loading...</div>;
+  useEffect(() => {
+    setAllTimeSlots(
+      schedule
+        ? Array.from(
+            new Set(
+              Object.keys(schedule).flatMap((day) => {
+                const daySchedule = schedule[day as keyof typeof schedule];
+                if (!Array.isArray(daySchedule)) return [];
+                return daySchedule.map(
+                  (entry) => `${entry?.startTime}-${entry?.endTime}`,
+                );
+              }),
+            ),
+          ).sort()
+        : [],
+    );
+  }, [schedule]);
 
-  const allTimeSlots = Array.from(
-    new Set(
-      Object.keys(schedule).flatMap((day) => {
-        const daySchedule = schedule[day as keyof typeof schedule];
-        if (!Array.isArray(daySchedule)) return [];
-        return daySchedule.map(
-          (entry) => `${entry?.startTime}-${entry?.endTime}`,
-        );
-      }),
-    ),
-  ).sort();
+  useEffect(() => {
+    setAllClassKeys(
+      tableSchedule
+        ? Object.keys(tableSchedule).flatMap((day) => {
+            const daySchedule =
+              tableSchedule[day as keyof typeof tableSchedule];
+            if (!Array.isArray(daySchedule)) return [];
+            return daySchedule.map((entry) =>
+              generateClassKey(
+                day as keyof typeof schedule,
+                entry.startTime,
+                entry.endTime,
+                entry.Teacher.id,
+                entry.Subject.id,
+              ),
+            );
+          })
+        : [],
+    );
+  }, [tableSchedule]);
+
+  // const allTimeSlots = schedule
+  //   ? Array.from(
+  //       new Set(
+  //         Object.keys(schedule).flatMap((day) => {
+  //           const daySchedule = schedule[day as keyof typeof schedule];
+  //           if (!Array.isArray(daySchedule)) return [];
+  //           return daySchedule.map(
+  //             (entry) => `${entry?.startTime}-${entry?.endTime}`,
+  //           );
+  //         }),
+  //       ),
+  //     ).sort()
+  //   : [];
 
   const toggleFixedClass = (classKey: string) => {
     setFixedClasses((prev) => {
@@ -105,9 +167,113 @@ export function SchoolCalendarGrid({ schoolId }: SchoolCalendarGridProps) {
     endTime: string,
     teacherId: string,
     subjectId: string,
-  ) {
+  ): ClassKey {
     return `${day}_${startTime}-${endTime}_${teacherId}_${subjectId}`;
   }
+
+  function handleDragEnd(event) {
+    console.log("handleDragEnd", event);
+    const { active, over } = event;
+    if (!active || !over) return;
+    if (!tableSchedule) return;
+
+    const activeId: ClassKey = active.id;
+    const [activeDay, activeStartandEndTime, activeTeacherId, activeSubjectId] =
+      activeId.split("_");
+    const [activeStartTime, activeEndTime] =
+      activeStartandEndTime?.split("-") || [];
+    const foundActiveIndex = tableSchedule[
+      activeDay as keyof typeof tableSchedule
+    ].findIndex(
+      (e) =>
+        e.startTime === activeStartTime &&
+        e.endTime === activeEndTime &&
+        e.Teacher.id === activeTeacherId &&
+        e.Subject.id === activeSubjectId,
+    );
+    if (foundActiveIndex === -1) return;
+    const overId: ClassKey = over.id;
+    const [overDay, overStartandEndTime, overTeacherId, overSubjectId] =
+      overId.split("_");
+    const [overStartTime, overEndTime] = overStartandEndTime?.split("-") || [];
+    const foundOverIndex = tableSchedule[
+      overDay as keyof typeof tableSchedule
+    ].findIndex(
+      (e) =>
+        e.startTime === overStartTime &&
+        e.endTime === overEndTime &&
+        e.Teacher.id === overTeacherId &&
+        e.Subject.id === overSubjectId,
+    );
+    if (foundOverIndex === -1) return;
+    const newTableSchedule = JSON.parse(JSON.stringify(tableSchedule));
+    const activeData =
+      tableSchedule[activeDay as keyof typeof tableSchedule][foundActiveIndex];
+    const overData =
+      tableSchedule[overDay as keyof typeof tableSchedule][foundOverIndex];
+    if (!activeData || !overData) return;
+    newTableSchedule[overDay as keyof typeof newTableSchedule][foundOverIndex] =
+      {
+        ...JSON.parse(
+          JSON.stringify(
+            tableSchedule[activeDay as keyof typeof tableSchedule][
+              foundActiveIndex
+            ],
+          ),
+        ),
+        startTime: overData.startTime,
+        endTime: overData.endTime,
+      };
+    newTableSchedule[activeDay as keyof typeof newTableSchedule][
+      foundActiveIndex
+    ] = {
+      ...JSON.parse(
+        JSON.stringify(
+          tableSchedule[overDay as keyof typeof tableSchedule][foundOverIndex],
+        ),
+      ),
+      startTime: activeData.startTime,
+      endTime: activeData.endTime,
+    };
+    setTableSchedule((items) => {
+      if (!items) return items;
+      return newTableSchedule;
+    });
+    setAllClassKeys(
+      Object.keys(newTableSchedule).flatMap((day) => {
+        const daySchedule =
+          newTableSchedule[day as keyof typeof newTableSchedule];
+        if (!Array.isArray(daySchedule)) return [];
+        return daySchedule.map((entry) =>
+          generateClassKey(
+            day as keyof typeof schedule,
+            entry.startTime,
+            entry.endTime,
+            entry.Teacher.id,
+            entry.Subject.id,
+          ),
+        );
+      }),
+    );
+  }
+
+  const sensors = useSensors(
+    useSensor(PointerSensor),
+    useSensor(KeyboardSensor, {
+      coordinateGetter: sortableKeyboardCoordinates,
+    }),
+  );
+
+  if (isLoading) return <div className="mt-5 text-center">Loading...</div>;
+  if (error) {
+    return <div className="mt-5 text-center">Error: {error.message}</div>;
+  }
+
+  if (!schedule || Object.keys(schedule).length === 0)
+    return <div className="mt-5 text-center">Loading...</div>;
+
+  if (!tableSchedule || Object.keys(tableSchedule).length === 0)
+    return <div className="mt-5 text-center">Loading...</div>;
 
   return (
     <div className="mt-5 overflow-x-auto">
@@ -151,83 +317,136 @@ export function SchoolCalendarGrid({ schoolId }: SchoolCalendarGridProps) {
         Atualizar
       </button>
 
-      <table className="min-w-full border border-gray-300 bg-white">
-        <thead className="bg-gray-800">
-          <tr>
-            <th className="border-b border-gray-300 px-4 py-2 text-white">
-              Time
-            </th>
-            {daysOfWeek.map((day) => (
-              <th
-                key={day}
-                className="border-b border-gray-300 px-4 py-2 text-white"
-              >
-                {day}
+      <DndContext
+        sensors={sensors}
+        collisionDetection={closestCenter}
+        onDragEnd={handleDragEnd}
+      >
+        <table className="min-w-full border border-gray-300 bg-white">
+          <thead className="bg-gray-800">
+            <tr>
+              <th className="border-b border-gray-300 px-4 py-2 text-white">
+                Time
               </th>
-            ))}
-          </tr>
-        </thead>
-        <tbody>
-          {allTimeSlots.map((timeSlot) => {
-            const [startTime, endTime] = timeSlot.split("-");
-            return (
-              <tr key={timeSlot} className="hover:bg-gray-100">
-                <td className="border-b border-gray-300 px-4 py-2">{`${startTime} - ${endTime}`}</td>
-                {daysOfWeek.map((day) => {
-                  const entry = schedule[day as keyof typeof schedule].find(
-                    (e) => e.startTime === startTime && e.endTime === endTime,
-                  );
-                  if (!entry) {
-                    return (
-                      <td
-                        key={day}
-                        className="border-b border-gray-300 px-4 py-2 text-center"
-                      >
-                        -
-                      </td>
-                    );
-                  }
-                  const classKey = generateClassKey(
-                    day,
-                    startTime as string,
-                    endTime as string,
-                    entry.Teacher.id,
-                    entry.Subject.id,
-                  );
-                  return (
-                    <td
-                      key={day}
-                      className="border-b border-gray-300 px-4 py-2 text-center"
-                    >
-                      <label>
-                        <CheckBox
-                          selected={fixedClasses.includes(classKey)}
-                          onClick={() => toggleFixedClass(classKey)}
-                        >
-                          <div className="flex flex-col items-start">
-                            <p>
-                              <span className="font-bold text-indigo-600">
-                                Professor:
-                              </span>{" "}
-                              {entry.Teacher.User.name}
-                            </p>
-                            <div>
-                              <span className="font-bold text-indigo-600">
-                                Matéria:
-                              </span>{" "}
-                              {entry.Subject.name}
-                            </div>
-                          </div>
-                        </CheckBox>
-                      </label>
-                    </td>
-                  );
-                })}
-              </tr>
-            );
-          })}
-        </tbody>
-      </table>
+              {daysOfWeek.map((day) => (
+                <th
+                  key={day}
+                  className="border-b border-gray-300 px-4 py-2 text-white"
+                >
+                  {day}
+                </th>
+              ))}
+            </tr>
+          </thead>
+          <tbody>
+            <SortableContext
+              items={allClassKeys}
+              strategy={rectSwappingStrategy}
+            >
+              {allTimeSlots.map((timeSlot) => {
+                const [startTime, endTime] = timeSlot.split("-");
+                return (
+                  <tr key={timeSlot} className="hover:bg-gray-100">
+                    <td className="border-b border-gray-300 px-4 py-2">{`${startTime} - ${endTime}`}</td>
+                    {daysOfWeek.map((day) => {
+                      const entry = tableSchedule[
+                        day as keyof typeof tableSchedule
+                      ].find(
+                        (e) =>
+                          e.startTime === startTime && e.endTime === endTime,
+                      );
+                      if (!entry) {
+                        return (
+                          <td
+                            key={day}
+                            className="border-b border-gray-300 px-4 py-2 text-center"
+                          >
+                            -
+                          </td>
+                        );
+                      }
+                      const classKey = generateClassKey(
+                        day,
+                        startTime as string,
+                        endTime as string,
+                        entry.Teacher.id,
+                        entry.Subject.id,
+                      );
+                      return (
+                        <ScheduledClass
+                          key={classKey}
+                          day={day}
+                          classKey={classKey}
+                          isSelected={fixedClasses.includes(classKey)}
+                          toggleFixedClass={toggleFixedClass}
+                          daySchedule={entry}
+                        />
+                      );
+                    })}
+                  </tr>
+                );
+              })}
+            </SortableContext>
+          </tbody>
+        </table>
+      </DndContext>
     </div>
+  );
+}
+
+interface ScheduledClassProps {
+  classKey: string;
+  isSelected: boolean;
+  toggleFixedClass: (classKey: string) => void;
+  day: DayOfWeek;
+  daySchedule: {
+    startTime: string;
+    endTime: string;
+    Teacher: Teacher & { User: User };
+    Subject: Subject;
+  };
+}
+
+function ScheduledClass({
+  day,
+  classKey,
+  isSelected,
+  toggleFixedClass,
+  daySchedule,
+}: ScheduledClassProps) {
+  const { attributes, listeners, setNodeRef, transform, transition } =
+    useSortable({ id: classKey });
+
+  const style = {
+    transform: CSS.Transform.toString(transform),
+    transition,
+  };
+  return (
+    <td
+      key={day}
+      className="border-b border-gray-300 px-4 py-2 text-center"
+      ref={setNodeRef}
+      style={style}
+      {...attributes}
+      {...listeners}
+    >
+      <label>
+        <CheckBox
+          selected={isSelected}
+          onClick={() => toggleFixedClass(classKey)}
+        >
+          <div className="flex flex-col items-start">
+            <p>
+              <span className="font-bold text-indigo-600">Professor:</span>{" "}
+              {daySchedule.Teacher.User.name}
+            </p>
+            <div>
+              <span className="font-bold text-indigo-600">Matéria:</span>{" "}
+              {daySchedule.Subject.name}
+            </div>
+          </div>
+        </CheckBox>
+      </label>
+    </td>
   );
 }
