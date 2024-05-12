2b479085c5041de67a09192b7ee22a1df3ecf68a
Davi de Carvalho <davi_carvalho96@hotmail.com>
2024-05-12 11:39:16 -0300

    adding clarity script

--- apps/nextjs/src/env.mjs
+++ apps/nextjs/src/env.mjs
@@ -5,16 +5,16 @@ import { z } from "zod";
  * built with invalid env vars.
  */
 const server = z.object({
-  DATABASE_URL: z.string().url(),
-  NODE_ENV: z.enum(["development", "test", "production"]),
-  CLERK_SECRET_KEY: z.string(),
-  SMTP_USER: z.string(),
-  SMTP_PASSWORD: z.string(),
-  SMTP_FROM_EMAIL: z.string(),
-  MINIO_ENDPOINT: z.string(),
-  MINIO_ACCESS_KEY: z.string(),
-  MINIO_SECRET_KEY: z.string(),
-  GLITCHTIP_DNS: z.string().optional(),
+	DATABASE_URL: z.string().url(),
+	NODE_ENV: z.enum(["development", "test", "production"]),
+	CLERK_SECRET_KEY: z.string(),
+	SMTP_USER: z.string(),
+	SMTP_PASSWORD: z.string(),
+	SMTP_FROM_EMAIL: z.string(),
+	MINIO_ENDPOINT: z.string(),
+	MINIO_ACCESS_KEY: z.string(),
+	MINIO_SECRET_KEY: z.string(),
+	GLITCHTIP_DNS: z.string().optional(),
 });
 
 /**
@@ -22,8 +22,9 @@ const server = z.object({
  * built with invalid env vars. To expose them to the client, prefix them with `NEXT_PUBLIC_`.
  */
 const client = z.object({
-  NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY: z.string(),
-  NEXT_PUBLIC_GLITCHTIP_DSN: z.string().optional(),
+	NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY: z.string(),
+	NEXT_PUBLIC_GLITCHTIP_DSN: z.string().optional(),
+	NEXT_PUBLIC_CLARITY_PROJECT_ID: z.string(),
 });
 
 /**
@@ -33,19 +34,19 @@ const client = z.object({
  * @type {Record<keyof z.infer<typeof server> | keyof z.infer<typeof client>, string | undefined>}
  */
 const processEnv = {
-  DATABASE_URL: process.env.DATABASE_URL,
-  NODE_ENV: process.env.NODE_ENV,
-  NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY:
-    process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY,
-  CLERK_SECRET_KEY: process.env.CLERK_SECRET_KEY,
-  SMTP_USER: process.env.SMTP_USER,
-  SMTP_PASSWORD: process.env.SMTP_PASSWORD,
-  SMTP_FROM_EMAIL: process.env.SMTP_FROM_EMAIL,
-  MINIO_ENDPOINT: process.env.MINIO_ENDPOINT,
-  MINIO_ACCESS_KEY: process.env.MINIO_ACCESS_KEY,
-  MINIO_SECRET_KEY: process.env.MINIO_SECRET_KEY,
-  GLITCHTIP_DNS: process.env.GLITCHTIP_DNS,
-  NEXT_PUBLIC_GLITCHTIP_DSN: process.env.NEXT_PUBLIC_GLITCHTIP_DSN,
+	DATABASE_URL: process.env.DATABASE_URL,
+	NODE_ENV: process.env.NODE_ENV,
+	NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY:
+		process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY,
+	CLERK_SECRET_KEY: process.env.CLERK_SECRET_KEY,
+	SMTP_USER: process.env.SMTP_USER,
+	SMTP_PASSWORD: process.env.SMTP_PASSWORD,
+	SMTP_FROM_EMAIL: process.env.SMTP_FROM_EMAIL,
+	MINIO_ENDPOINT: process.env.MINIO_ENDPOINT,
+	MINIO_ACCESS_KEY: process.env.MINIO_ACCESS_KEY,
+	MINIO_SECRET_KEY: process.env.MINIO_SECRET_KEY,
+	GLITCHTIP_DNS: process.env.GLITCHTIP_DNS,
+	NEXT_PUBLIC_GLITCHTIP_DSN: process.env.NEXT_PUBLIC_GLITCHTIP_DSN,
 };
 
 // Don't touch the part below
@@ -60,36 +61,36 @@ const merged = server.merge(client);
 let env = /** @type {MergedOutput} */ (process.env);
 
 if (!!process.env.SKIP_ENV_VALIDATION == false) {
-  const isServer = typeof window === "undefined";
+	const isServer = typeof window === "undefined";
 
-  const parsed = /** @type {MergedSafeParseReturn} */ (
-    isServer
-      ? merged.safeParse(processEnv) // on server we can validate all env vars
-      : client.safeParse(processEnv) // on client we can only validate the ones that are exposed
-  );
+	const parsed = /** @type {MergedSafeParseReturn} */ (
+		isServer
+			? merged.safeParse(processEnv) // on server we can validate all env vars
+			: client.safeParse(processEnv) // on client we can only validate the ones that are exposed
+	);
 
-  if (parsed.success === false) {
-    console.error(
-      "❌ Invalid environment variables:",
-      parsed.error.flatten().fieldErrors,
-    );
-    throw new Error("Invalid environment variables");
-  }
+	if (parsed.success === false) {
+		console.error(
+			"❌ Invalid environment variables:",
+			parsed.error.flatten().fieldErrors,
+		);
+		throw new Error("Invalid environment variables");
+	}
 
-  env = new Proxy(parsed.data, {
-    get(target, prop) {
-      if (typeof prop !== "string") return undefined;
-      // Throw a descriptive error if a server-side env var is accessed on the client
-      // Otherwise it would just be returning `undefined` and be annoying to debug
-      if (!isServer && !prop.startsWith("NEXT_PUBLIC_"))
-        throw new Error(
-          process.env.NODE_ENV === "production"
-            ? "❌ Attempted to access a server-side environment variable on the client"
-            : `❌ Attempted to access server-side environment variable '${prop}' on the client`,
-        );
-      return target[/** @type {keyof typeof target} */ (prop)];
-    },
-  });
+	env = new Proxy(parsed.data, {
+		get(target, prop) {
+			if (typeof prop !== "string") return undefined;
+			// Throw a descriptive error if a server-side env var is accessed on the client
+			// Otherwise it would just be returning `undefined` and be annoying to debug
+			if (!isServer && !prop.startsWith("NEXT_PUBLIC_"))
+				throw new Error(
+					process.env.NODE_ENV === "production"
+						? "❌ Attempted to access a server-side environment variable on the client"
+						: `❌ Attempted to access server-side environment variable '${prop}' on the client`,
+				);
+			return target[/** @type {keyof typeof target} */ (prop)];
+		},
+	});
 }
 
 export { env };
--- apps/nextjs/src/pages/_app.tsx
+++ apps/nextjs/src/pages/_app.tsx
@@ -19,6 +19,15 @@ const MyApp: AppType = ({ Component, pageProps: { ...pageProps } }) => {
         defer
       />
       <GoogleTagManager gtmId="G-7ND9Y9SETT" />
+      <Script id="clarity-script" strategy="afterInteractive">
+        {`
+            (function(c,l,a,r,i,t,y){
+                c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+                t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+                y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+            })(window, document, "clarity", "script", "${process.env.NEXT_PUBLIC_CLARITY_PROJECT_ID}");
+          `}
+      </Script>
       <TRPCReactProvider>
         <Component {...pageProps} />
       </TRPCReactProvider>
