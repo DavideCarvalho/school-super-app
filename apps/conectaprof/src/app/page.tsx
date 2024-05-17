8e96332fdc55a062e2e23e08e6dcc3df461128fb
Davi de Carvalho <davi_carvalho96@hotmail.com>
2024-05-17 19:52:49 -0300

    Tirando o "nós"

--- apps/conectaprof/src/app/page.tsx
+++ apps/conectaprof/src/app/page.tsx
@@ -78,7 +78,7 @@ export default function Component() {
             <div className="mt-8 flex flex-col items-center justify-center space-y-4 text-center">
               <div className="space-y-2">
                 <h2 className="text-3xl font-bold tracking-tighter text-orange-600 sm:text-5xl">
-                  Quem somos nós?
+                  Quem somos?
                 </h2>
                 <p className="max-w-[900px] text-gray-500 md:text-xl/relaxed lg:text-base/relaxed xl:text-xl/relaxed dark:text-gray-400">
                   Olá, somos os criadores do Anuá, uma plataforma de gestão
