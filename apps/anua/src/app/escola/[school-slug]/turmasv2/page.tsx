7e0e4e46b7b547361c15fa47a029208dde265a3f
Davi de Carvalho <davi_carvalho96@hotmail.com>
2024-05-31 18:44:00 -0300

    criando pagina de turmas v2

---
+++ apps/anua/src/app/escola/[school-slug]/turmasv2/page.tsx
@@ -0,0 +1,46 @@
+import { headers } from "next/headers";
+import Link from "next/link";
+import { dehydrate, HydrationBoundary } from "@tanstack/react-query";
+
+import { Button } from "@acme/ui/button";
+
+import { api, createSSRHelper } from "~/trpc/server";
+
+export default async function SubjectsPage({
+  params,
+}: {
+  params: { "school-slug": string };
+  searchParams?: { [key: string]: string | string[] | undefined };
+}) {
+  const requestHeaders = headers();
+  const xUrl = requestHeaders.get("x-url");
+  if (!xUrl) throw new Error("unreachable");
+  const url = new URL(xUrl);
+  if (!url.searchParams.has("page")) {
+    url.searchParams.set("page", "1");
+  }
+  if (!url.searchParams.has("limit")) {
+    url.searchParams.set("limit", "10");
+  }
+  const school = await api.school.bySlug({ slug: params["school-slug"] });
+  if (!school) throw new Error("School not found");
+  const helper = await createSSRHelper();
+  await helper.subject.allBySchoolId.prefetch({
+    page: Number(url.searchParams.get("page")),
+    limit: Number(url.searchParams.get("limit")),
+  });
+  await helper.subject.countAllBySchoolId.prefetch();
+  const dehydratedState = dehydrate(helper.queryClient);
+  return (
+    <HydrationBoundary state={dehydratedState}>
+      <div className="mb-4 flex items-center justify-between">
+        <h2 className="text-xl font-semibold">Turmas</h2>
+        <Link
+          href={`${url.pathname}?${url.searchParams.toString()}#adicionar-turma`}
+        >
+          <Button>Adicionar Turma</Button>
+        </Link>
+      </div>
+    </HydrationBoundary>
+  );
+}
