3e51e94ca86763ca2241177e57daef5cdeeb7e90
Davi de Carvalho <davi_carvalho96@hotmail.com>
2024-06-05 11:29:23 -0300

    separando entre administrativo e cantina

--- apps/anua/src/layouts/SchoolLayout.tsx
+++ apps/anua/src/layouts/SchoolLayout.tsx
@@ -56,9 +56,13 @@ const routes: Route[] = [
     name: "Funcionários",
     href: (params) => `/escola/${params.schoolSlug}/funcionarios`,
   },
+];
+
+const canteenRoutes: Route[] = [
   {
     name: "Itens da cantina",
-    href: (params) => `/escola/${params.schoolSlug}/itens-cantina`,
+    href: (params) =>
+      `/escola/${params.schoolSlug}/cantina/${params.canteenId}/itens-cantina`,
   },
 ];
 
@@ -240,6 +244,41 @@ export function SchoolLayout({ children }: SchoolLayoutProps) {
                         className={cn(
                           "group flex items-center rounded-lg px-4 py-2.5 text-sm font-medium text-gray-900 transition-all duration-200 hover:bg-gray-200",
                           matchesPathname(routeHref, pathname)
+                            ? "bg-gray-200"
+                            : "",
+                        )}
+                      >
+                        <svg
+                          className="mr-4 h-5 w-5 flex-shrink-0"
+                          xmlns="http://www.w3.org/2000/svg"
+                          fill="none"
+                          viewBox="0 0 24 24"
+                          stroke="currentColor"
+                          strokeWidth="2"
+                        >
+                          <title>{route.name}</title>
+                          <path
+                            strokeLinecap="round"
+                            strokeLinejoin="round"
+                            d="M9 19v-6a2 2 0 00-2-2H5a2 2 0 00-2 2v6a2 2 0 002 2h2a2 2 0 002-2zm0 0V9a2 2 0 012-2h2a2 2 0 012 2v10m-6 0a2 2 0 002 2h2a2 2 0 002-2m0 0V5a2 2 0 012-2h2a2 2 0 012 2v14a2 2 0 01-2 2h-2a2 2 0 01-2-2z"
+                          />
+                        </svg>
+                        {route.name}
+                      </Link>
+                    );
+                  })}
+                  <p className="px-4 text-xs font-semibold uppercase tracking-widest text-gray-400">
+                    Cantina
+                  </p>
+                  {canteenRoutes.map((route) => {
+                    const routeHref = route.href({ schoolSlug, canteenId });
+                    return (
+                      <Link
+                        key={route.name}
+                        href={routeHref}
+                        className={cn(
+                          "group flex items-center rounded-lg px-4 py-2.5 text-sm font-medium text-gray-900 transition-all duration-200 hover:bg-gray-200",
+                          matchesPathname(routeHref, pathname ?? "")
                             ? "bg-gray-200"
                             : "",
                         )}
