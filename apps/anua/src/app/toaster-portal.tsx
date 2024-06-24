b323674eac1608db3d485c7637350287d43476d7
Davi de Carvalho <davi_carvalho96@hotmail.com>
2024-06-24 12:09:44 -0300

    Arrumando toaster

--- apps/anua/src/app/toaster-portal.tsx
+++ apps/anua/src/app/toaster-portal.tsx
@@ -5,13 +5,10 @@ import { createPortal } from "react-dom";
 import { Toaster } from "react-hot-toast";
 
 export function ToasterPortal() {
-  const [isSSR, setSSR] = useState(false);
+  const [isSSR, setSSR] = useState(true);
   useEffect(() => {
     setSSR(typeof window === "undefined");
   }, []);
   if (isSSR) return null;
-  return createPortal(
-    <Toaster position="top-center" />,
-    globalThis.document.body,
-  );
+  return createPortal(<Toaster position="top-center" />, window.document.body);
 }
