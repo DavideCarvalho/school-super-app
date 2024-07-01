3ff7920a3d3f2c37333293610d319b7fbd093852
Davi de Carvalho <davi_carvalho96@hotmail.com>
2024-07-01 10:48:00 -0300

    removendo stepper

--- apps/anua/src/app/escola/[school-slug]/administrativo/periodos-letivos/novo-periodo-letivo/containers/calendar-form/calendar-form.client.tsx
+++ apps/anua/src/app/escola/[school-slug]/administrativo/periodos-letivos/novo-periodo-letivo/containers/calendar-form/calendar-form.client.tsx
@@ -1,20 +1,12 @@
 "use client";
 
-import type { DateRange, DayDateProps } from "react-day-picker";
+import type { DayDateProps } from "react-day-picker";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { useForm } from "react-hook-form";
 import { z } from "zod";
 
-import { cn } from "@acme/ui";
 import { Button } from "@acme/ui/button";
 import { Calendar } from "@acme/ui/calendar";
-import {
-  Card,
-  CardContent,
-  CardDescription,
-  CardHeader,
-  CardTitle,
-} from "@acme/ui/card";
 import { DateRangePicker } from "@acme/ui/date-range-picker";
 import { Form } from "@acme/ui/form";
 import { Label } from "@acme/ui/label";
@@ -146,40 +138,30 @@ export function CalendarFormClient() {
             </Popover>
           </div>
 
-          <Card>
-            <CardHeader>
-              <CardTitle>Calendário</CardTitle>
-              <CardDescription>
-                Visualize o período escolar com os dias de feriado e final de
-                semana.
-              </CardDescription>
-            </CardHeader>
-            <CardContent>
-              <h1>Você tem {daysWithClasses} dias com aulas</h1>
-              <Calendar
-                mode="multiple"
-                selected={allDates}
-                onSelect={() => {}}
-                onDayClick={() => {}}
-                components={{
-                  Day: (props: DayDateProps) => {
-                    const isHolidayDay = isHoliday(props.day.date);
-                    return (
-                      <div
-                        {...props.rootProps}
-                        style={{
-                          backgroundColor: isHolidayDay ? "orange" : undefined,
-                        }}
-                        onClick={() => {}}
-                      >
-                        {props.day.date.getDate()}
-                      </div>
-                    );
-                  },
-                }}
-              />
-            </CardContent>
-          </Card>
+          <h1>Você tem {daysWithClasses} dias com aulas</h1>
+          <Calendar
+            mode="multiple"
+            selected={allDates}
+            onSelect={() => {}}
+            onDayClick={() => {}}
+            components={{
+              Day: (props: DayDateProps) => {
+                const isHolidayDay = isHoliday(props.day.date);
+                return (
+                  <div
+                    {...props.rootProps}
+                    style={{
+                      backgroundColor: isHolidayDay ? "orange" : undefined,
+                    }}
+                    onClick={() => {}}
+                  >
+                    {props.day.date.getDate()}
+                  </div>
+                );
+              },
+            }}
+          />
+          <Button>Salvar</Button>
         </form>
       </Form>
     </div>
