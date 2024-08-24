049e05df8159ea2a10fc616bc8e03682611b500a
Davi de Carvalho <davi_carvalho96@hotmail.com>
2024-08-24 17:06:18 -0300

    adicionando inteligencia no dashboard

--- apps/anua/src/app/escola/[school-slug]/containers/inteligence-carousel/inteligence-carousel.client.tsx
+++ apps/anua/src/app/escola/[school-slug]/containers/inteligence-carousel/inteligence-carousel.client.tsx
@@ -1,16 +1,101 @@
 "use client";
 
+import UsersIcon from "@heroicons/react/24/outline/UsersIcon";
+
+import { Card, CardContent, CardHeader, CardTitle } from "@acme/ui/card";
 import {
   Carousel,
   CarouselContent,
+  CarouselItem,
   CarouselNext,
   CarouselPrevious,
 } from "@acme/ui/carousel";
 
+import { api } from "~/trpc/react";
+
 export function InteligenceCarouselClient() {
+  const { data: printRequestsNotApprovedCloseToDueDate } =
+    api.inteligence.getPrintRequestsNotApprovedCloseToDueDate.useQuery();
+  const { data: printRequestsToPrintToday } =
+    api.inteligence.getPrintRequestsToPrintToday.useQuery();
+  const { data: printRequestsOwnerUserNeedToReview } =
+    api.inteligence.getPrintRequestsOwnerUserNeedToReview.useQuery();
   return (
     <Carousel>
       <CarouselContent>
+        {printRequestsNotApprovedCloseToDueDate &&
+        printRequestsNotApprovedCloseToDueDate?.length > 0 ? (
+          <CarouselItem className="md:basis-1/2 lg:basis-1/3">
+            <Card>
+              <CardHeader className="flex flex-row items-center justify-between pb-2">
+                <CardTitle className="text-sm font-medium">Impressão</CardTitle>
+                <UsersIcon className="h-4 w-4 text-muted-foreground" />
+              </CardHeader>
+              <CardContent>
+                <div className="text-lg font-bold">
+                  {printRequestsNotApprovedCloseToDueDate.length}{" "}
+                  {printRequestsNotApprovedCloseToDueDate.length === 1
+                    ? "Impressão"
+                    : "Impressões"}
+                </div>
+                <p className="text-sm text-muted-foreground">
+                  {printRequestsNotApprovedCloseToDueDate.length === 1
+                    ? "Está em risco de não ser impressas a tempo"
+                    : "Estão em risco de não ser impressas a tempo"}
+                </p>
+              </CardContent>
+            </Card>
+          </CarouselItem>
+        ) : null}
+
+        {printRequestsToPrintToday && printRequestsToPrintToday?.length > 0 ? (
+          <CarouselItem className="md:basis-1/2 lg:basis-1/3">
+            <Card>
+              <CardHeader className="flex flex-row items-center justify-between pb-2">
+                <CardTitle className="text-sm font-medium">Impressão</CardTitle>
+                <UsersIcon className="h-4 w-4 text-muted-foreground" />
+              </CardHeader>
+              <CardContent>
+                <div className="text-lg font-bold">
+                  {printRequestsToPrintToday.length}{" "}
+                  {printRequestsToPrintToday.length === 1
+                    ? "Impressão"
+                    : "Impressões"}
+                </div>
+                <p className="text-sm text-muted-foreground">
+                  {printRequestsToPrintToday.length === 1
+                    ? "Precisa ser impressa hoje"
+                    : "Precisam ser impressas hoje"}
+                </p>
+              </CardContent>
+            </Card>
+          </CarouselItem>
+        ) : null}
+
+        {printRequestsOwnerUserNeedToReview &&
+        printRequestsOwnerUserNeedToReview?.length > 0 ? (
+          <CarouselItem className="md:basis-1/2 lg:basis-1/3">
+            <Card>
+              <CardHeader className="flex flex-row items-center justify-between pb-2">
+                <CardTitle className="text-sm font-medium">Impressão</CardTitle>
+                <UsersIcon className="h-4 w-4 text-muted-foreground" />
+              </CardHeader>
+              <CardContent>
+                <div className="text-lg font-bold">
+                  {printRequestsOwnerUserNeedToReview.length}{" "}
+                  {printRequestsOwnerUserNeedToReview.length === 1
+                    ? "Impressão"
+                    : "Impressões"}
+                </div>
+                <p className="text-sm text-muted-foreground">
+                  {printRequestsOwnerUserNeedToReview.length === 1
+                    ? "Precisa ser revisada"
+                    : "Precisam ser revisadas"}
+                </p>
+              </CardContent>
+            </Card>
+          </CarouselItem>
+        ) : null}
         <CarouselPrevious />
         <CarouselNext />
       </CarouselContent>
